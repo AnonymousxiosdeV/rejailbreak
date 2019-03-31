583f7b6bd27ab080c7d7fe5838597f0fb73277cc
AnonymousXiOSdeV <AnonymousxiosdeV@users.noreply.github.com>
2019-03-31 00:59:39 -0700

    Final

--- rejailbreak.sh
+++ rejailbreak.sh
@@ -1,97 +1,154 @@
-#!/bin/sh
-
-# Make sure to run as root#
+#!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
 echo "This script must be run as root"
 exit 1
 fi
 
-#Progress Bar Function#
+CODE_SAVE_CURSOR="\033[s"
+CODE_RESTORE_CURSOR="\033[u"
+CODE_CURSOR_IN_SCROLL_AREA="\033[1A"
+COLOR_FG="\e[30m"
+COLOR_BG="\e[42m"
+COLOR_BG_BLOCKED="\e[43m"
+RESTORE_FG="\e[39m"
+RESTORE_BG="\e[49m"
 
-function prg_bar ()
-{
+PROGRESS_BLOCKED="false"
+TRAPPING_ENABLED="false"
+TRAP_SET="false"
 
-    local size_bar=$1 pct=$2 num_hash=0 num_point=0
-
-    if [ $pct -gt 100 ] || [ $pct -lt 0 ]
-    then
-        return 1
+setup_scroll_area() {
+    if [ "$TRAPPING_ENABLED" = "true" ]; then
+        trap_on_interrupt
     fi
 
-    let "num_hash = ($size_bar * $pct)/100"
-    let 'num_point = size_bar - num_hash'
-
-    echo -ne " Progress ["
-    
-    while true;
-    do
-        while [ $num_hash != 0 ]
-        do
-            echo -ne "#"
-            let 'num_hash -= 1'
-        done
-
-        while [ $num_point != 0 ]
-        do
-            echo -ne "."
-            let 'num_point -= 1'
-        done
-
-        if [ $pct = 100 ]
-        then
-            echo -ne "] $pct% Done !\n"
-        else
-            echo -ne "] $pct%\r"
-        fi
-      
-        break
-
-    done
-
-    return 0
+    lines=$(tput lines)
+    let lines=$lines-1
+    echo -en "\n"
+    echo -en "$CODE_SAVE_CURSOR"
+    echo -en "\033[0;${lines}r"
+    echo -en "$CODE_RESTORE_CURSOR"
+    echo -en "$CODE_CURSOR_IN_SCROLL_AREA"
+    draw_progress_bar 0
 }
 
-
-function Update ()
-{
-apt -y update >>/dev/null 2>&1
+destroy_scroll_area() {
+    lines=$(tput lines)
+    echo -en "$CODE_SAVE_CURSOR"
+    echo -en "\033[0;${lines}r"
+    echo -en "$CODE_RESTORE_CURSOR"
+    echo -en "$CODE_CURSOR_IN_SCROLL_AREA"
+    clear_progress_bar
+    echo -en "\n\n"
+    if [ "$TRAP_SET" = "true" ]; then
+        trap - INT
+    fi
 }
 
-function Upgrade ()
-{
-apt -y --fix-missing upgrade >>/dev/null 2>&1
+draw_progress_bar() {
+    percentage=$1
+    lines=$(tput lines)
+    let lines=$lines
+    echo -en "$CODE_SAVE_CURSOR"
+    echo -en "\033[${lines};0f"
+    tput el
+    PROGRESS_BLOCKED="false"
+    print_bar_text $percentage
+    echo -en "$CODE_RESTORE_CURSOR"
 }
 
-#Array list of packages to install#
+block_progress_bar() {
+    percentage=$1
+    lines=$(tput lines)
+    let lines=$lines
+    echo -en "$CODE_SAVE_CURSOR"
+    echo -en "\033[${lines};0f"
+    tput el
+    PROGRESS_BLOCKED="true"
+    print_bar_text $percentage
+    echo -en "$CODE_RESTORE_CURSOR"
+}
+
+clear_progress_bar() {
+    lines=$(tput lines)
+    let lines=$lines
+    echo -en "$CODE_SAVE_CURSOR"
+    echo -en "\033[${lines};0f"
+    tput el
+    echo -en "$CODE_RESTORE_CURSOR"
+}
+
+print_bar_text() {
+    local percentage=$1
+    local cols=$(tput cols)
+    let bar_size=$cols-17
+
+    local color="${COLOR_FG}${COLOR_BG}"
+    if [ "$PROGRESS_BLOCKED" = "true" ]; then
+        color="${COLOR_FG}${COLOR_BG_BLOCKED}"
+    fi
+
+    let complete_size=($bar_size*$percentage)/100
+    let remainder_size=$bar_size-$complete_size
+    progress_bar=$(echo -ne "["; echo -en "${color}"; printf_new "#" $complete_size; echo -en "${RESTORE_FG}${RESTORE_BG}"; printf_new "." $remainder_size; echo -ne "]");
+
+    echo -ne " Progress ${percentage}% ${progress_bar}"
+}
+
+enable_trapping() {
+    TRAPPING_ENABLED="true"
+}
+
+trap_on_interrupt() {
+    TRAP_SET="true"
+    trap cleanup_on_interrupt INT
+}
+
+cleanup_on_interrupt() {
+    destroy_scroll_area
+    exit
+}
+
+printf_new() {
+    str=$1
+    num=$2
+    v=$(printf "%-${num}s" "$str")
+    echo -ne "${v// /$str}"
+}
 
 Array=(basic-cmds com.a3tweaks.flipswitch com.cokepokes.inutt com.cydiageek.skinnysettings com.iwazowski.iweppro5 com.midnightchips.localhostssh com.opa334.safariplus com.repo.xarold.com.cocoatop64 com.repo.xarold.com.cydown com.rpetrich.rocketbootstrap com.tigisoftware.appdatamanager curl cydia.com.iphonecake.appcake6 cydia.kiiimo.org.filzafilemanager cydia.kiiimo.org.icleanerpro10 git grep htop idv.aqua.igameguardian.ios12 jp.ashikase.libpackageinfo jp.ashikase.techsupport ld64 ldid less libactivator libssh2 libssl1.0 libxml2 llvm-clang lzma make nano net.angelxwind.appsyncunified odcctool openssh org.cydia.kiimo.crackicleanerpro perl preferenceloader readline rsync sed uuid com.jakeashacks.jtool wget Flex3beta adv-cmds)
 
-
-sleep ".001"
-prg_bar 30 1
-Update
-prg_bar 30 5
-
-COUNTER=5
-for a in "${Array[@]}";
-do
-apt -yq install $a >/dev/null 2>&1
-wait
-let COUNTER=COUNTER+1
-prg_bar 30 $COUNTER
+main() {
+    enable_trapping
+    setup_scroll_area
+    draw_progress_bar 1
+    apt-get -y update >/dev/null 2>&1
+    draw_progress_bar 5
+    wait
+   
+ COUNTER=5
+    for a in "${Array[@]}";
+    do
+    apt-get -y --allow-unauthenticated install $a >/dev/null 2>&1
+    wait
+    let COUNTER=COUNTER+1
+    draw_progress_bar $COUNTER
 done
-
-
-prg_bar 30 95
-Update
-prg_bar 30 97
-Upgrade
-prg_bar 30 100
+    apt-get -y update >/dev/null 2>&1
+    wait
+    draw_progress_bar 95
+    apt-get -y --fix-missing upgrade >/dev/null 2>&1
+    wait
+    draw_progress_bar 97
+    uicache
+    draw_progress_bar 100
 sleep "2"
-echo "[*] Running  uicache"
+destroy_scroll_area
 echo "[*] Running  Respring"
-sleep "2"
+sleep "5"
 killall -9 SpringBoard
+exit 0   
+}
 
-exit 0
+main
